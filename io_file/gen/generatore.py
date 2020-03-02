c3a31078d4b62f19bccef84d2a4b3390da0782ba
Romeo Rizzi <romeo.rizzi@univr.it>
2020-03-02 18:56:40 +0100

    sistemato generazione per problema io

--- io_file/gen/generatore.py
+++ io_file/gen/generatore.py
@@ -9,28 +9,18 @@ usage="""Generatore di "prova".
 
 Parametri:
 * N (numero)
-* S (seed)
 
 Constraint:
 * 1 <= N <= %d
 """ % (MAXN)
 
 
-def run(N):
-    print N
-
-
 if __name__ == "__main__":
-    if len(argv) != 3:
+    if len(argv) != 2:
         print usage
         exit(1)
 
-    N, S = map(int, argv[1:])
+    N = int(argv[1])
     assert (-MAXN <= N <= MAXN)
-
-    # su seed non positivo copia un input di esempio dal .tex
-    if S <= 0:
-        print extract_input()[-S],
-        exit(0)
-    run(N)
+    print N
 
